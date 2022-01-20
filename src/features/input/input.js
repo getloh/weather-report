42e96b1072a02b4f54194dc7db7f446369878a26
GetLoh <88935273+getloh@users.noreply.github.com>
2022-01-20 13:07:18 +0000

    Styled the searchbox, added simple animation

--- src/features/input/input.js
+++ src/features/input/input.js
@@ -1,10 +1,14 @@
 import { setQuery, setUnit} from "./inputSlice";
 import { useDispatch, useSelector } from 'react-redux'
 import {Weather} from '../../api/weatherapi.js'
+import './input.css'
+import search from '../../img/search.png'
 
 export const Input = () => {
     const dispatch = useDispatch();
     const state = useSelector(state => state);
+    const searchBar = document.getElementById("searchBox");
+
     
     const handleInput = (event) => {
         dispatch(setQuery(event.target.value))
@@ -12,7 +16,8 @@ export const Input = () => {
 
     const handleFetch = (event) => {
         // dispatch the fetch to API via thunk
-        Weather.getWeather(state.input.query)
+        Weather.getWeather(state.input.query);
+        document.getElementById("searchBox").style.animation = "slide 1s ease forwards";
     }
 
     const handleChangeUnit = (event) => {
@@ -32,14 +37,20 @@ export const Input = () => {
 
     return (
         <div>
-          {state.main.fetched === true ? <div className="unit-buttons">
-            <button onClick={handleChangeUnit}>°{state.input.unit}</button>
-          </div> : null}
+          
 
-          <div>
-          <input className="input-field" placeholder="London, UK" onChange={handleInput} type='text' />
-          <button className="button" onClick={handleFetch}>Grab the weather!</button>
+          <div className="search-wrap" >
+            <div className="search-box" id="searchBox" >
+              <input className="search" placeholder="London, UK" onChange={handleInput} type='text' />
+              <button className="button" onClick={handleFetch}><img className="searchimage" src={search}/></button>
+              
+            </div>
           </div>
+
+          {state.main.fetched === true ? <div className="unit-buttons">
+            <button className="btn" onClick={handleChangeUnit}>°{state.input.unit}</button>
+          </div> : null}
+          
         </div>
       );
 }
